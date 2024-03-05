b886b3f2358590f5a0ed79aee0f6b2418811ce57
Terry <olaterry208@yahoo.com>
2024-03-05 20:03:05 +0000

    added card funtion using bootstrap

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -20,18 +20,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Project = ({ title, description, imageUrl, projectUrl, exploreUrl }) => {
+const Project = ({ title, description, imageUrl, projectUrl, GithubUrl }) => {
   return (
-    <div className="card mx-auto" style={{ width: '18rem' }}>
+    <div className="card mx-auto my-3" style={{ width: '18rem' }}>
       <img src={imageUrl} className="card-img-top" alt={title} />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{description}</p>
-        <a href={projectUrl} className="btn btn-primary" target="_blank" rel="noopener noreferrer">View Project</a>
-        <a href={exploreUrl} className="btn btn-secondary">Explore More</a>
+        <a href={GithubUrl} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Github</a>
+        <a href={projectUrl} className="btn btn-secondary" target="_blank" rel="noopener noreferrer">View Project</a>
       </div>
     </div>
   );
 };
 
 export default Project;
+
--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -9,12 +9,16 @@ const projectData = [
     description:
       "Its an application that allow user to manage a nomal workday, the app displays the current day at the top and allows users to scroll through standard business hours. Users can easily add, save, and view events for each hour of the workday.",
     imageUrl: "./images/work-scheduler.gif",
+    projectUrl: "https://terr0y.github.io/Work_Day_Scheduler/", 
+    GithubUrl: "https://github.com/Terr0y/Work_Day_Scheduler" 
   },
   {
     title: "Weather Forecast",
     description:
       "Users can search for any city to get detailed weather data such as temperature, humidity, wind speed, and more.",
     imageUrl: "./images/weather.dashboard.png",
+    projectUrl: "https://terr0y.github.io/Weather_Forecast/", 
+    GithubUrl: "https://github.com/Terr0y/Weather_Forecast" 
   },
   
 ];
