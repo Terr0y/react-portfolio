4ff20023c04f284ca62aa335fdbdfd64463f4f18
Terry <olaterry208@yahoo.com>
2024-03-05 20:30:38 +0000

    added contactpage container

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -9,7 +9,7 @@ const ContactPage = () => {
             <div className="card-body">
               <i className="fa fa-phone"></i>
               <h4 className="text-uppercase">Phone</h4>
-              <div>(123) 456-7890</div> {/* your phone number */}
+              <div>(123) 456-7890</div> {/*contact phone number */}
             </div>
           </div>
         </div>
@@ -18,7 +18,7 @@ const ContactPage = () => {
             <div className="card-body">
               <i className="fa fa-envelope"></i>
               <h4 className="text-uppercase">Email</h4>
-              <div>olaterry208@yahoo.com</div> {/* your actual email */}
+              <div>olaterry208@yahoo.com</div> {/* contact email */}
             </div>
           </div>
         </div>
